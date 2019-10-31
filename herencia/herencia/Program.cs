25d571785fc415073e8d0f3952a48feb1c5f1f0f
erisjimver <erisjinver@gmail.com>
2019-10-31 11:39:50 -0500

    Agregando herencia por parametros

--- herencia/herencia/Program.cs
+++ herencia/herencia/Program.cs
@@ -6,7 +6,74 @@ namespace herencia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Caballo Bebieca = new Caballo("Bebieca");
+            Humano Israel = new Humano("Israel");
+            Gorila Goku = new Gorila("Goku");
+            Bebieca.getNombre();
+            Israel.getNombre();
+            Goku.getNombre();
+        }
+    }
+
+    class Mamiferos
+    {
+
+        public Mamiferos(String nombre)
+        {
+            nombreSerVivo = nombre;
+        }
+
+        public void respirar()
+        {
+            Console.WriteLine("Puedo respirar");
+        }
+
+        public void cuidarCrias()
+        {
+            Console.WriteLine("Puedo cuidar a mis crias");
+        }
+
+        public void getNombre()
+        {
+            Console.WriteLine("El nombre del ser vivo es: "+nombreSerVivo);
+        }
+
+        private string nombreSerVivo;
+    }
+
+    class Caballo:Mamiferos
+    {
+        public Caballo(String nombreCaballo) : base(nombreCaballo)
+        {
+
+        }
+        public void galopar()
+        {
+            Console.WriteLine("Soy capaz de galopar");
+        }
+    }
+
+    class Humano:Mamiferos
+    {
+        public Humano(String nombreHumano) : base(nombreHumano)
+        {
+
+        }
+        public void pensar()
+        {
+            Console.WriteLine("Soy capaz de pensar");
+        }
+    }
+
+    class Gorila:Mamiferos
+    {
+        public Gorila(String nombreGorila) : base(nombreGorila)
+        {
+
+        }
+        public void trepar()
+        {
+            Console.WriteLine("Soy capaz de trepar");
         }
     }
 }
