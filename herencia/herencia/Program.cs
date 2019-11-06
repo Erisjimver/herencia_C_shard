4b5eb9eba4978772904e265e7cc6c982b4d404db
erisjimver <erisjinver@gmail.com>
2019-11-05 19:15:12 -0500

    interfaces

--- herencia/herencia/Program.cs
+++ herencia/herencia/Program.cs
@@ -9,9 +9,15 @@ namespace herencia
             Caballo Bebieca = new Caballo("Bebieca");
             Humano Israel = new Humano("Israel");
             Gorila Goku = new Gorila("Goku");
+            Ballena fliper = new Ballena("Fliper");
             Bebieca.getNombre();
             Israel.getNombre();
             Goku.getNombre();
+            fliper.respirar();
+            fliper.nadar();
+            Console.WriteLine(Israel.deporte());
+            Bebieca.numeroPatas();
+            Bebieca.deporte();
 
             Mamiferos[] mamiferos = new Mamiferos[3];
             mamiferos[0] = Bebieca;
@@ -59,31 +65,80 @@ namespace herencia
         private string nombreSerVivo;
     }
 
-    class Caballo:Mamiferos
+    class Ballena : Mamiferos
+    {
+        public Ballena(String nombreBallena) : base(nombreBallena)
+        {
+
+        }
+
+        public void nadar()
+        {
+            Console.WriteLine("Soy capaz de nadar");
+        }
+
+        public override void pensar()//pensamiento instintivo
+        {
+            Console.WriteLine("Soy capaz de pensar, como pez");
+        }
+    }
+
+
+
+    class Caballo:Mamiferos,InterfaceMmiferosTerrestres,InterfaceAnimalesDeportes
     {
         public Caballo(String nombreCaballo) : base(nombreCaballo)
         {
 
         }
+
+        public string deporte()
+        {
+            return "Deporte de la saltacion"; 
+        }
+
         public void galopar()
         {
             Console.WriteLine("Soy capaz de galopar");
         }
+
+        public int numeroPatas()
+        {
+            int numero = 4;
+            return numero;
+
+        }
+
+        public bool olimpico()
+        {
+            return true;
+        }
     }
 
-    class Humano:Mamiferos
+    class Humano:Mamiferos,InterfaceAnimalesDeportes
     {
         public Humano(String nombreHumano) : base(nombreHumano)
         {
 
         }
+
+        public string deporte()
+        {
+            return "hago el deporte de la pelotacion";
+        }
+
+        public bool olimpico()
+        {
+            return true;
+        }
+
         public override void pensar()
         {
             Console.WriteLine("Soy capaz de pensar, Avanzado");
         }
     }
 
-    class Gorila:Mamiferos
+    class Gorila:Mamiferos,InterfaceMmiferosTerrestres
     {
         public Gorila(String nombreGorila) : base(nombreGorila)
         {
@@ -98,5 +153,23 @@ namespace herencia
         {
             Console.WriteLine("Soy capaz de pensar, intermedio");
         }
+
+        public int numeroPatas()
+        {
+            int numero = 2;
+            return numero;
+
+        }
+    }
+
+    interface InterfaceMmiferosTerrestres
+    {
+        int numeroPatas();
+    }
+
+    interface InterfaceAnimalesDeportes
+    {
+        string deporte();
+        Boolean olimpico();
     }
 }
